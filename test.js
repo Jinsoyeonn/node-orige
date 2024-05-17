dd59f494e1f6329178bc3b9999ca39d2fb5833c8
Jinsoyeonn <wlsdmsquf@nate.com>
2024-05-17 16:17:24 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,14 @@
-    
-var express = require("express");
-var app = express();
+import { Bootpay } from '@bootpay/backend-js'
 
-app.get("/", (req, res) => {
-    res.send(
-        "<p>안녕하세요</p>"
-    )
+Bootpay.setConfiguration({
+    application_id: '65fbb57800be04001b0635c7',
+    private_key: 'oBByqBtRMsAyNJwrGmSZ5IjgUjnidAG+0sAzYhnEjgA='
 })
-app.listen(3000, (err)=> {
-    console.log(3000);
-})
+
+try {
+    const response = await Bootpay.getAccessToken()
+    console.log(response)
+} catch (e) {
+    // 발급 실패시 오류
+    console.log(e)
+}
